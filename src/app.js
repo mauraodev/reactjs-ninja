fec96996a516fd71df551c8be42678075a6bbfb5
Mauro Nunes <mauraoemau@hotmail.com>
2019-07-21 23:53:14 -0300

    Life cycle mount and unmount

--- src/app.js
+++ src/app.js
@@ -1,21 +1,33 @@
 'use strict'
 
 import React from "react"
+import Timer from "./timer";
 
 class App extends React.Component {
   constructor () {
+    console.log('constructor')
     super()
     this.state = {
-      text: 'Mauro'
+     showTimer: true
     }
   }
 
+  componentWillMount () {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount () {
+    console.log('componentDidMount')
+  }
+
   render() {
+    console.log('render')
     return (
-      <div className="container" onClick={() => this.setState({
-        text: 'Outro texto'
-      })}>
-        {this.state.text}
+      <div>
+        {this.state.showTimer && <Timer />}
+        <button onClick={() => {
+          this.setState({ showTimer: false})
+        }}>Show/Hide Timer</button>
       </div>
     )
   }
