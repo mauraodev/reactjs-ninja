10272fdc0f659eade83efcb5ac4c9e3895b20617
Mauro Nunes <mauraoemau@hotmail.com>
2019-12-08 19:08:54 -0300

    Select

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      value: 'Valor inicial',
-      checked: false,
+      select: 3,
     }
   }
 
@@ -15,28 +14,15 @@ class App extends Component {
     return (
       <div>
         <form>
-          <input type="text" value={this.state.value} onChange={(e) => {
-            console.log(e)
-            this.setState({
-              value: e.target.value
-            })
-          }}/>
-
-          <label>
-            <input type="checkbox"
-              value="my-checkbox"
-              checked={this.state.checked}
-              onChange={(e) => {
-                this.setState({
-                  checked: !this.state.checked
-                })
-              }}
-              />
-            Checkbox
-          </label>
-
-          <input type="radio" name="rd" value="rd1" defaultChecked/> Radio 1
-          <input type="radio" name="rd" value="rd2" /> Radio 2
+          <select multiple value={this.state.select}
+            onChange={(e) => {
+              this.setState({ select: e.target.value })
+            }}
+            >
+            <option value="1">Opção 1</option>
+            <option value="2">Opção 2</option>
+            <option value="3">Opção 3</option>
+          </select>
         </form>
       </div>
     )
