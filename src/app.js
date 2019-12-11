caa158387d6ae68e67f98446d278a24847d4c0bf
Mauro Nunes <mauraoemau@hotmail.com>
2019-12-10 23:38:47 -0300

    Textarea

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      select: 3,
+      value: 'texto\nquebra de linha',
     }
   }
 
@@ -14,15 +14,7 @@ class App extends Component {
     return (
       <div>
         <form>
-          <select multiple value={this.state.select}
-            onChange={(e) => {
-              this.setState({ select: e.target.value })
-            }}
-            >
-            <option value="1">Opção 1</option>
-            <option value="2">Opção 2</option>
-            <option value="3">Opção 3</option>
-          </select>
+          <textarea value={ this.state.value }/>
         </form>
       </div>
     )
